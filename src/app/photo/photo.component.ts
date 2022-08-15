519d3fdb8b81d75859a287ddde22d02cf022e5f4
Anderlan pc1 <anderlan.dev@gmail.com>
2022-08-15 00:54:03 -0300

    criando o primeiro componente

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: './photo.component.html',
+  })
+
+export class PhotoComponent {
+
+  title = 'alurapic';
+  description = 'Leão';
+  url= 'https://st.depositphotos.com/1020341/4233/i/450/depositphotos_42333899-stock-photo-portrait-of-huge-beautiful-male.jpg';
+}
